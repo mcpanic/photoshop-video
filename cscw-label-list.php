680ae51a40e36fc21458570ab1baf8def0d0f3df
Juho Kim <truepanic@gmail.com>
2013-05-26 14:25:08 -0400

    line

--- cscw-label-list.php
+++ cscw-label-list.php
@@ -13,7 +13,8 @@ include "conn.php";
 		// $video['task_id'] = "1001";
 	while ($video = $result->fetch_assoc()) {
 		$url = "cscw-label.php?vid=" . $video['id'] . "&tid=" . $video['task_id'];
-		echo $video['id'] . "\t" . $video['title'] . "\t" . "<a href='$url'>$url</a>";	}
+		echo $video['id'] . "\t" . $video['title'] . "\t" . "<a href='$url' target='_blank'>$url</a><br/>";	
+	}
 ?>
 </body>
 </html>
