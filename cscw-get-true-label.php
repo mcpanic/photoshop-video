65a696bab271ee632647cce09c9127109c0ca6d9
Juho Kim <truepanic@gmail.com>
2013-05-26 17:17:08 -0400

    truth

--- cscw-get-true-label.php
+++ cscw-get-true-label.php
@@ -1,22 +1,6 @@
 <?php
 
-//include "conn.php";
-
-// NOTE: The following 'define' settings are machine and server-specific.
-// Make sure to configure them correctly before starting ToolScape.
-
-define("FFMPEG_PATH", "/opt/local/bin/ffmpeg");	// path of ffmpeg
-
-define("DB_HOST", "50.116.6.114");	// MySQL host name
-define("DB_USERNAME", "toolscape");	// MySQL username
-define("DB_PASSWD", "toolscape");	// MySQL password
-define("DB_NAME", "video_learning");	// MySQL database name. vt.sql uses the default video_learning name. So be careful.
-
-$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWD, DB_NAME);
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-
+include "conn.php";
 
 $cid_list = array();
 $mid_list = array();
